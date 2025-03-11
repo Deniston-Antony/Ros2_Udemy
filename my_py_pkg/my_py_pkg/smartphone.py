e5aefdf74a851ea367b1a01d0685213c3f549c45
Deniston_Antony <deniston2k@gmail.com>
2025-03-11 21:03:11 +0530

    main

--- my_py_pkg/my_py_pkg/smartphone.py
+++ my_py_pkg/my_py_pkg/smartphone.py
@@ -6,13 +6,18 @@ from example_interfaces.msg import String
 class SmartphoneNode(Node): # MODIFY NAME
     def __init__(self):
         super().__init__("smartphone") # MODIFY NAME
-        self.subscriber_ = self.create_subscription(String,"",10)
+        self.subscriber_ = self.create_subscription(
+            String,"robot_news", self.callback_robot_news, 10)
+        self.get_logger().info("Smartphone has been started.")
+   
+    def callback_robot_news(self, msg: String):
+        self.get_logger().info(msg.data) # msg.data is from the publisher
 
-    def main(args=None):
-        rclpy.init(args=args)
-        node = SmartphoneNode() # MODIFY NAME
-        rclpy.spin(node)
-        rclpy.shutdown()
+def main(args=None):
+    rclpy.init(args=args)
+    node = SmartphoneNode() # MODIFY NAME
+    rclpy.spin(node)
+    rclpy.shutdown()
 
 
 if __name__ == " __main__ ":
